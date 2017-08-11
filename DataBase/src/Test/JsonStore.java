b39895e2c0ac5608d709b4d14b5c6fad82b0d9ca
geraldvm <gerald-_16@hotmail.com>
2017-08-10 21:27:26 -0600

    Clase Json Store
    Clase encargada de crear Carpetas

---
+++ DataBase/src/Test/JsonStore.java
@@ -0,0 +1,26 @@
+package Test;
+import java.io.File;
+
+public class JsonStore {
+    private String name; //Nombre de la carpeta
+
+    public void setName(String name) {
+        this.name = name;
+        this.create();
+
+    }
+    public String getName()
+    {
+        return name;
+    }
+
+    private void create()
+    { //Crear Carpetas
+        //String directory = "C:\\Users\\geral\\Documents\\PHOTO\\TEST";
+        String directory = System.getProperty("user.dir")+"\\"+this.getName(); //Directorio
+        File newStore = new File(directory); //Crea el objeto
+        newStore.mkdirs();
+
+    }
+
+}
---
+++ DataBase/src/Test/Run.java
@@ -0,0 +1,11 @@
+package Test;
+import Test.JsonStore;
+
+public class Run {
+
+    public static void main(String args[])
+    {
+        JsonStore n = new JsonStore();
+        n.setName("C2");
+    }
+}
