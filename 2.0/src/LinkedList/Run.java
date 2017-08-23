7a540b46fd559600f1b1a81db79908e0d37e6f54
geraldvm <gerald-_16@hotmail.com>
2017-08-23 05:26:04 -0600

    Lista Enlazada Simple

--- 2.0/src/LinkedList/Run.java
+++ 2.0/src/LinkedList/Run.java
@@ -1,22 +1,62 @@
 package LinkedList;
 
+import LinkedList.CircularList.CircularList;
+import LinkedList.DoubleList.DoubleList;
+import LinkedList.SimpleList.SimpleList;
+
 public class Run {
 
     public static void main(String[] args) {
 
-        Simple listaSimple = new Simple();
-        listaSimple.addLast(1);
-        listaSimple.addLast("b");
-        listaSimple.addLast(3);
-        listaSimple.addLast(3.0);
-        listaSimple.show();
-        listaSimple.erase("b");
-        listaSimple.search(1);
-        listaSimple.update(3, "hello");
-        listaSimple.erase(3.0);
-        listaSimple.erase(3.0);
-        listaSimple.show();
+        SimpleList a = new SimpleList();
+
+        System.out.println("*****************\n SimpleList");
+        a.addLast(1);
+        a.addLast("a");
+        a.addLast("b");
+        a.addLast("c");
+        a.addLast("d");
+
+        a.erase(1);
+        a.erase("c");
+        a.update("d","hello");
+
+        //a.erase("hello");
+        a.addLast("x");
+        a.erase("x");
+        a.show();
+
+        //*********************************************************************
+        System.out.println("*****************\n DoubleList");
+        DoubleList b = new DoubleList();
+        b.addFirst("e");
+        b.addLast(5);
+        b.addLast(6);
+        b.show();
+        // a.showReverse();
+        System.out.println(b.search(25));
+        b.search(23);
+
+        //*********************************************************************
+        System.out.println("*****************\n CircularDList");
+        CircularList c = new CircularList();
+
+        c.addLast(1);
+        c.addLast(2);
+        c.addLast(3);
+        c.addLast(4);
+        c.addLast(5);
+        c.addLast(6);
+        c.addLast(7);
+        c.addLast(8);
+        c.show();
+        // a.showReverse();
+        System.out.println(c.search(4));
+        c.eraseLast();
+        c.show();
+
+        c.search(23);
+
 
     }
-
 }
