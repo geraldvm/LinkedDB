b5b4d90e4e1f0fdd74093af04d57dd5b654ff2fc
geraldvm <gerald-_16@hotmail.com>
2017-08-23 01:29:13 -0600

    Listas

--- 2.0/src/LinkedList/Node.java
+++ 2.0/src/LinkedList/Node.java
@@ -25,14 +25,4 @@ public class Node {
     }
 }
 
-public class NodeDouble extends Node{
-    private NodeDouble previous;
 
-    public void setPrevious(Node previous) {
-        this.previous = previous;
-    }
-
-    public NodeDouble getPrevious() {
-        return previous;
-    }
-}
---
+++ 2.0/src/LinkedList/NodeDouble.java
@@ -0,0 +1,13 @@
+package LinkedList;
+
+public class NodeDouble extends Node{
+    private NodeDouble previous;
+
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    }
+
+    public NodeDouble getPrevious() {
+        return previous;
+    }
+}
