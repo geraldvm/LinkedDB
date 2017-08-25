4b82e1b1b311d79f10c133015198fec60e2511b0
geraldvm <gerald-_16@hotmail.com>
2017-08-24 23:50:17 -0600

    JSON STORE CREAR CARPETAS

---
+++ 1.0/src/JsonStore/Run.java
@@ -0,0 +1,15 @@
+package JsonStore;
+
+
+
+public class Run {
+    public static void main(String[] args) {
+        JsonStore n = new JsonStore();
+        n.setName("C2");
+        n.setName("C3");
+        n.setName("4");
+        n.setName("4");
+        n.getStores();
+
+    }
+}
