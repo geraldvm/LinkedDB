ec1867e9f9d16a7726ba5a9dc902bad116655dc3
geraldvm <gerald-_16@hotmail.com>
2017-09-01 01:29:18 -0600

    main
    Test de JSONDocument

--- 1.0/src/Documents/RUN.java
+++ 1.0/src/Documents/RUN.java
@@ -1,57 +1,21 @@
 package Documents;
 
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Iterator;
 
 public class RUN {
 
+    public static void main(String[] args) {
+        JSONDocument a = new JSONDocument("StoreA","Tabla1");
+        a.createAttribute("ID",2,true,false);
+        a.createAttribute("name",1,false,true);
+        a.createAttribute("name",2,true,false);
 
-    /*public static void main(String[] args) {
-
-        JSONParser parser = new JSONParser();
-
-        try {
-
-            Object obj = parser.parse(new FileReader("C:\\Users\\geral\\Desktop\\test.json"));
-
-            JSONObject jsonObject = (JSONObject) obj;
-            //jsonObject.get("Store2");
-            //jsonObject.put("HELLO","NEHHH");
-            //jsonObject.writeJSONString();
-            //System.out.println(jsonObject);
-
-            //System.out.println(jsonObject.get("Store2"));
-
-            JSONObject attL = (JSONObject) jsonObject.get("Store2");
-            JSONObject x = new JSONObject();
-            x.put("Key","LOW");
-            x.put("Type", "FEO");
-            attL.replace("nameAT2",x);
-
-            System.out.println(attL.keySet());
-            JSONObject attS = (JSONObject) attL.get("nameAT2");
+        a.createFile();
 
 
 
-            jsonObject.replace("Store2",attL);
-            //System.out.println(jsonObject);
+    }
 
 
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }*/
 }
