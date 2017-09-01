ddd44cd7c5e85bdb526df398d0ce207649b72f98
geraldvm <gerald-_16@hotmail.com>
2017-08-31 22:55:37 -0600

    Verificaciones crear stores
    Se agregaron comprobaciones de los store fisicos y los de memoria

--- 1.0/src/JsonStore/JsonStore.java
+++ 1.0/src/JsonStore/JsonStore.java
@@ -1,8 +1,8 @@
 package JsonStore;
 import java.io.File;
-import LinkedList.DoubleList.DoubleList;
+import LinkedList.DoubleList.*;
 
-import javax.jws.soap.InitParam;
+
 import Load.LoadFile;
 
 public class JsonStore {
@@ -35,8 +35,8 @@ public class JsonStore {
     }
 
     public void createCarpet() {
-        //name nombre de la carpeta
-        this.createCarpet_aux();
+
+        createCarpet_aux();
 
     }
 
@@ -50,29 +50,44 @@ public class JsonStore {
         this.registry.show();
     }
 
-    private boolean createCarpet_aux(String name)
+    private void createCarpet_aux()
     { //Crear Carpetas
         //String directory = "C:\\Users\\geral\\Documents\\PHOTO\\TEST";
+        //Lee los stores en memoria fisica
         LoadFile storeMemory = new LoadFile("s");
         storeMemory.loadStores();
-        JsonStore oldStore = new JsonStore(storeMemory.getStoreList());
-        /*if(oldStore.tiene(registry.getitem))
-            else
-                */
-        if (this.registry.search(name)){
-            System.out.println("ERROR Escoja otro Nombre");
-            return false;
-        }
-        else
+        DoubleList oldStore = storeMemory.getStoreList();
+        Node temp;
+        oldStore.show();
+        System.out.println("//////////////////////////");
+
+        for(int i=0;i<this.registry.length();i++)//Recorre la lista con cambios
         {
-            //String directory = System.getProperty("user.dir")+"\\"+name; //Directorio
-            String directory = System.getProperty("C:\\Users\\geral\\Desktop\\LINKED"+name); //Directorio
-            File newStore = new File(directory); //Crea el objeto
-            newStore.mkdirs();
-            return true;
+            temp = this.registry.find(i);
+
+            /*System.out.println(temp.getItem());
+            System.out.println(oldStore.search(temp.getItem()));
+            */
+            if(!oldStore.search(temp.getItem()))//Si no existia ese store lo crea
+            {
+                System.out.println(temp.getItem());
+                createCarpet_aux(temp.getItem().toString());
+            }
+            else
+            {
+                //System.out.println("errr");
+                //System.out.println(temp.getItem());
+            }
+
         }
 
-
+    }
+    private void createCarpet_aux(String directory)
+    {
+        //String path = System.getProperty("user.dir")+"\\STORES\\"+directory; //Directorio
+        String path = "C:\\Users\\geral\\Desktop\\LINKED\\"+directory; //Directorio
+        File newStore = new File(path); //Crea el objeto
+        newStore.mkdirs();
     }
 
 }
