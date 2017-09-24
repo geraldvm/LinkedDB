fca8b18a72cf3c6c58cbd6d80b6c9a6d448c898e
geraldvm <gerald-_16@hotmail.com>
2017-09-23 23:42:07 -0600

    Tree

--- 7.0/src/sample/MainController.java
+++ 7.0/src/sample/MainController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 
 import java.net.URL;
@@ -17,32 +18,41 @@ public class MainController implements Initializable{
     @FXML
     TreeView<String> treeView;
 
-    Image folder = new Image(getClass().getResourceAsStream("/img/folder.png"));
+    Image folder = new Image(getClass().getResourceAsStream("/img/store.png"));
+    Image file = new Image(getClass().getResourceAsStream("/img/a.png"));
+    Image object = new Image(getClass().getResourceAsStream("/img/object.png"));
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        TreeItem<String> root = new TreeItem<>("Root", new ImageView(folder));
-        TreeItem<String> StoreA = new TreeItem<>("StoreA");
-        TreeItem<String> StoreB = new TreeItem<>("StoreB");
-        TreeItem<String> StoreC = new TreeItem<>("StoreC");
+        TreeItem<String> root = new TreeItem<>("Root");
+        TreeItem<String> StoreA = new TreeItem<>("StoreA", new ImageView(folder));
+        TreeItem<String> StoreB = new TreeItem<>("StoreB", new ImageView(folder));
+        TreeItem<String> StoreC = new TreeItem<>("StoreC", new ImageView(folder));
         root.getChildren().addAll(StoreA,StoreB,StoreC);
 
-        TreeItem<String> Doc1 = new TreeItem<>("JsonDoc1");
-        TreeItem<String> Doc2 = new TreeItem<>("JsonDoc2");
-        TreeItem<String> Doc3 = new TreeItem<>("JsonDoc3");
+        TreeItem<String> Doc1 = new TreeItem<>("JsonDoc1",new ImageView(file));
+        TreeItem<String> Doc2 = new TreeItem<>("JsonDoc2",new ImageView(file));
+        TreeItem<String> Doc3 = new TreeItem<>("JsonDoc3",new ImageView(file));
         StoreA.getChildren().addAll(Doc1,Doc2,Doc3);
+        //StoreA.setExpanded(true);
+        //Expandir El nodo
 
-        TreeItem<String> ObjA = new TreeItem<>("ObjectA");
-        TreeItem<String> ObjB = new TreeItem<>("ObjectB");
-        TreeItem<String> ObjC = new TreeItem<>("ObjectC");
-        Doc1.getChildren().addAll(ObjA,ObjC,ObjC);
+        TreeItem<String> ObjA = new TreeItem<>("ObjectA",new ImageView(object));
+        TreeItem<String> ObjB = new TreeItem<>("ObjectB",new ImageView(object));
+        TreeItem<String> ObjC = new TreeItem<>("ObjectC",new ImageView(object));
+        Doc1.getChildren().addAll(ObjA,ObjC,ObjB);
 
         treeView.setRoot(root);
+    }
 
-
-
+    public void mouseClick(MouseEvent mouseEvent){
+        if(mouseEvent.getClickCount()==2){
+            TreeItem<String> item = treeView.getSelectionModel().getSelectedItem();
+            System.out.println(item.getValue());
+        }
 
 
     }
+
 }
