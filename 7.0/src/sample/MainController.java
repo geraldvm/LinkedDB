b284a34f62dbd35c49b28ff61a60d30768ab9fff
geraldvm <gerald-_16@hotmail.com>
2017-09-30 06:50:48 -0600

    h
    h

--- 7.0/src/sample/MainController.java
+++ 7.0/src/sample/MainController.java
@@ -68,6 +68,8 @@ public class MainController implements Initializable{
             } else {
                 showT.setDisable(true);
             }
+        }else {
+            showT.setDisable(false);
         }
 
 
